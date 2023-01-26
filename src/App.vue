00025e2cff9899f3af6f8ea1eab89d61edb52f30
Chaz Kondo <chuckeekondo@gmail.com>
2023-01-25 14:36:31 -1000

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="loading" class="loader">Loading...</div>
+  <div class="main">
+    <img class="hello" alt="Vue logo" src="./assets/logo.png" />
+    <ColorApp
+      msg="Welcome to Your Vue.js App"
+      v-on:loadingCheck="updateLoading()"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ColorApp from "./components/ColorApp.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ColorApp,
+  },
+  data: function () {
+    return {
+      loading: true,
+      updateLoading: function () {
+        this.loading = false;
+      },
+    };
+  },
+};
 </script>
 
 <style>
-#app {
+#app,
+html,
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+.hello {
+  margin-top: 50px;
+}
+
+.loader {
+  color: white;
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  background: rgba(0, 0, 0, 0.3);
+  font-size: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
---
+++ src/components/ColorApp.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="container">
+      <div>
+        <div>Color 1</div>
+        <div id="circle-1" class="circle" :style="color1"></div>
+        <div id="label-1">{{ color1.text }}</div>
+      </div>
+      <div>
+        <div>Color 2</div>
+        <div id="circle-2" class="circle" :style="color2"></div>
+        <div id="label-2">{{ color2.text }}</div>
+      </div>
+      <div>
+        <div>Mixed</div>
+        <div id="circle-mixed" class="circle" :style="mixed"></div>
+        <div id="label-mixed">{{ mixed.text }}</div>
+      </div>
+      <div>
+        <div>Secret</div>
+        <div id="circle-secret" class="circle" :style="secret"></div>
+        <div id="label-secret">{{ secret.text }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ColorApp",
+  data: function () {
+    return {
+      color1: {
+        text: "#000000",
+        backgroundColor: "black",
+      },
+      color2: {
+        text: "#000000",
+        backgroundColor: "black",
+      },
+      mixed: {
+        text: "#000000",
+        backgroundColor: "black",
+      },
+      secret: {
+        text: "#000000",
+        backgroundColor: "black",
+      },
+    };
+  },
+  async mounted() {
+    fetch("https://shrouded-mountain-08630.herokuapp.com/api")
+      .then((res) => res.json())
+      .then((data) => {
+        this.color1.backgroundColor = data.color1;
+        this.color2.backgroundColor = data.color2;
+        this.mixed.backgroundColor = data.mixed;
+        this.secret.backgroundColor = data.secret;
+
+        this.color1.text = data.color1;
+        this.color2.text = data.color2;
+        this.mixed.text = data.mixed;
+        this.secret.text = data.secret;
+
+        this.$emit("loadingCheck");
+      })
+      .catch((err) => {
+        console.log("fetch err", err);
+        this.$emit("loadingCheck");
+      });
+  },
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.circle {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  margin: 5px;
+  background: black;
+}
+
+.container div {
+  text-align: center;
+  padding: 20px;
+}
+
+pre {
+  background: #fafafa;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 0.5em;
+  max-width: 400px;
+}
+
+ul > pre {
+  max-width: 320px;
+}
+</style>
